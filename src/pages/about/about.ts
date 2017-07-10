8640df6f1eb3e36181c2c62d3662cdc0969ceb9d
Davi Rodrigues <davisouzamachado@outlook.com>
2017-07-10 20:58:01 -0300

    primeiro commit
    commit do app-ionic-teste2 (menos o node_modules)

---
+++ src/pages/about/about.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
+
+
+export class MyPage {
+  constructor(public loadingCtrl: LoadingController) {
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+  }
+}
